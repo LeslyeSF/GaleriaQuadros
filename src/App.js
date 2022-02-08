37e45762bfb4882caadd3c68b343b0fd3e6f3195
Leslye Soares Ferreira <leslyesoares@gmail.com>
2022-02-08 20:48:53 -0300

    initializing routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import HomePage from "./pages/HomePage";
 import GlobalStyle from "./styles";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App(){
   
-  
   return(
     <>
-      <GlobalStyle/>
-      <h1>oii</h1>
+    <GlobalStyle/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/signin" element={<HomePage/>}/>
+        <Route path="/signup" element={<HomePage/>}/>
+        <Route path="/product/:idProduct" element={<HomePage/>}/>
+      </Routes>
+    </BrowserRouter>      
     </>
   );
 }
