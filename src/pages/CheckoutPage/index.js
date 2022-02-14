7b202cd0172d7c0a241a178f69a7b00ffe53ce5a
Leslye Soares Ferreira <leslyesoares@gmail.com>
2022-02-14 18:44:35 -0300

    login style

--- src/pages/CheckoutPage/index.js
+++ src/pages/CheckoutPage/index.js
@@ -3,7 +3,8 @@ import { useNavigate } from "react-router";
 import {Header} from "../../components/Header/Header.js";
 import UserContext from "../../contexts/UserContext";
 import { checkout } from "../../services/galeriaQuadros.js";
-import { tokenVerify } from "../../services/tokenService.js";
+import ModalError from "../../shared/ModalError.js";
+import ModalSuccess from "../../shared/ModalSuccess.js";
 import {CheckoutScreen, OptionContainer, Option, Title, Value, Amount, ButtonCheckout} from "./style";
 
 export default function CheckoutPage(){
@@ -12,30 +13,30 @@ export default function CheckoutPage(){
     list:[],
     amount: 0
   });
-  const navigate = useNavigate();  
+  const navigate = useNavigate();
+
+  const [modalError, setModalError] = useState(false);
+  const [modalSuccess, setModalSuccess] = useState(false);
+  const [message, setMessage] = useState(false);  
 
   useEffect(()=>{
-    tokenVerify(navigate, token);
     let list=[], amount = 0;
     list = productSelected?.map((date, index)=>{
-      let value = date.value.replace(",",".");
-      amount += parseFloat(value);
+      amount += parseFloat(date.price);
       return(
         <Option key={index}>
-          <img src={date.linkImg} alt={date.title}/>
+          <img src={date.imageUrl} alt={date.name}/>
           <div>
-            <span>{date.title}</span>
-            <p>{date.author}</p>
+            <span>{date.name}</span>
+            <p>{date.artistName}</p>
           </div>
           <div>
             <p>valor:</p>
-            <Value>{date.value}</Value>
+            <Value>{date.price}</Value>
           </div>
         </Option>
       );
     });
-    amount = ""+amount;
-    amount = amount.replace(".",",");
     setProducts({
       list: list,
       amount: amount
@@ -43,16 +44,18 @@ export default function CheckoutPage(){
   },[]);
 
   function handleCheckout(){
-    const body = {
-      list: productSelected,
-      amount: products.amount
-    };
+    const body = {...products};
     const promise = checkout(body,token);
     promise.then(()=>{
-      navigate("/");
+      setMessage('Compra realizada com sucesso');
+      setModalSuccess(true);
+      setTimeout(() => {
+        navigate('/');
+      }, 2000);
     });
-    promise.catch((err)=>{
-      console.log(err.response);
+    promise.catch(()=>{
+      setMessage('Ocorreu um erro, tente mais tarde')
+      setModalError(true);
     });
   }
   return(
@@ -64,9 +67,20 @@ export default function CheckoutPage(){
       </OptionContainer>
       <Amount>
         <p>Valor total da compra:</p>
-        <Value>{products.amount.toFixed(2)}</Value>
+        <Value>{products.amount}</Value>
       </Amount>
       <ButtonCheckout onClick={handleCheckout}>Finalizar Compra</ButtonCheckout>
+      {
+        modalError ?
+          <ModalError message={ message } setModal={ setModalError } />
+        : ''
+      }
+
+      {
+        modalSuccess ?
+          <ModalSuccess message={ message } />
+        : ''
+      }
     </CheckoutScreen>
   );
 }
